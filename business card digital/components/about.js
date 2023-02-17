b298acc3cc6081709fbe12562c3dabbfc4981901
Matthew Parmer <111148574+Pigiron55@users.noreply.github.com>
2023-02-17 15:35:32 -0500

    Add files via upload

---
+++ business card digital/components/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function About() {
+    return (
+        <div>
+            <h3>About Me</h3>
+            <p>I am a junior front end web developer. I recently completed the Scrimba online Front-End Developer career path course. I learned Javascript, HTML, CSS and React through project based learning curriculum. This year I decided to make a career change into Web Development and have jumped into this skillset with both feet. I have been studying and building various projects for the last six months.</p>
+        </div>
+    )
+}
---
+++ business card digital/components/socials.js
@@ -0,0 +1,26 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Socials() {
+    return (
+        <footer className="footer">
+            <ul>
+                <li>
+                    <a href="https://codepen.io">
+                        <i className="fab fa-codepen"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/matthew-parmer-74ba87248/">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/Pigiron55">
+                        <i className="fab fa-github"></i>
+                    </a>
+                </li>
+            </ul>
+        </footer>
+    )
+}
